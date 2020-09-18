174abc28ebf721a701f7cd54e1ae0866a8ac82fb
AvianLearning <iaradcliffe@hotmail.com>
2020-09-18 20:18:56 +0100

    Created initial tests for Song class

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,7 +4,7 @@ from classes.guest import Guest
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-        self.guest1 = Guest("Colin the Crooner", 62, 34.23)
+        self.guest_1 = Guest("Colin the Crooner", 62, 34.23)
 
     def test_can_find_guest_by_name(self):
-        self.assertEqual("Colin the Crooner", self.guest1.name)
+        self.assertEqual("Colin the Crooner", self.guest_1.name)
--- tests/song_test.py
+++ tests/song_test.py
@@ -3,5 +3,13 @@ from classes.song import Song
 
 class TestSong(unittest.TestCase):
 
-   def setUp(self):
-       pass
+    def setUp(self):
+        self.song_1 = Song("Isn't it Pythonic?", "Alanis Morissette")
+        self.song_2 = Song("Little Red Rooster", "Jimi Hendrix")
+
+    def test_can_find_song_by_name(self):
+        self.assertEqual("Isn't it Pythonic?", self.song_1.title)
+    
+    def test_can_find_song_by_artist(self):
+        self.assertEqual("Alanis Morissette", self.song_1.title)
+        self.assertEqual("Jimi Hendrix", self.song_2.title)
