6cda92e713842802fa6aebd386462bd9a338d051
AvianLearning <iaradcliffe@hotmail.com>
2020-09-18 19:54:59 +0100

    Created failing test to find room name

--- tests/room_test.py
+++ tests/room_test.py
@@ -3,5 +3,8 @@ from classes.room import Room
 
 class TestRoom(unittest.TestCase):
 
-   def setUp(self):
-       pass
+    def setUp(self):
+       self.room1 = Room("Lizard Lounge")
+
+    def test_room_has_name(self):
+        self.assertEqual("Lizard Lounge", self.room1.name)
