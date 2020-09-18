91ad16159bc27e1d8646d6b169954e05ddc801ec
AvianLearning <iaradcliffe@hotmail.com>
2020-09-18 20:09:46 +0100

    Added failing test to find guest by name

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,5 +3,8 @@ from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
 
-   def setUp(self):
-       pass
+    def setUp(self):
+        self.guest1 = Guest("Colin the Crooner", 62, 34.23)
+
+    def test_can_find_guest_by_name(self):
+        self.assertEqual("Colin the Crooner", self.guest1.name)
