9ffd3245d47131dd2f5b994405dc8f8299a62480
AvianLearning <iaradcliffe@hotmail.com>
2020-09-19 21:03:05 +0100

    Created test to check karaoke bar can collect entry fee - failed test

--- classes/karaoke_bar.py
+++ classes/karaoke_bar.py
@@ -1,2 +1,6 @@
 class KaraokeBar:
-    pass
+    def __init__(self, name, room_list, till, entry_fee):
+        self.name = name
+        self.room_list = room_list
+        self.till = till
+        self.entry_fee = entry_fee
--- tests/karaoke_bar_test.py
+++ tests/karaoke_bar_test.py
@@ -1,5 +1,18 @@
 import unittest
+from classes.karaoke_bar import KaraokeBar
 from classes.room import Room 
 
 class TestKaraokeBar(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.room_1 = Room("Lizard Lounge", 3)
+        self.room_2 = Room("Viper's Nest", 10)
+        self.room_3 = Room("Clucking Hell", 15)
+        room_list = [self.room_1, self.room_2, self.room_3]
+        self.karaoke_bar = KaraokeBar("The Hit Re-Factory", room_list, 100.00, 10.00)
+
+    def test_karaoke_bar_can_collect_entry_fee(self):
+        self.karaoke_bar.collect_entry_fee(self.entry_fee)
+        self.assertEqual(110.00, self.karaoke_bar.till) 
+
+        
