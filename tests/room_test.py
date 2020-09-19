9170502d914ecb9cdb272f0953310e8dd0c647b1
AvianLearning <iaradcliffe@hotmail.com>
2020-09-19 12:09:20 +0100

    Added failing test to check can add songs to room song list

--- tests/room_test.py
+++ tests/room_test.py
@@ -1,6 +1,7 @@
 import unittest
 from classes.room import Room 
 from classes.guest import Guest
+from classes.song import Song
 
 class TestRoom(unittest.TestCase):
 
@@ -9,6 +10,10 @@ class TestRoom(unittest.TestCase):
        self.guest_1 = Guest("Colin the Crooner", 62, 34.23)
        self.guest_2 = Guest("Sadie the Screamer", 38, 62.89)
        self.guest_3 = Guest("Tuneless Wanda", 22, 29.99)
+       self.song_1 = Song("Isn't it Pythonic?", "Alanis Morissette")
+       self.song_2 = Song("Little Red Rooster", "Jimi Hendrix")
+       self.song_3 = Song("Computer Love", "Kraftwerk")
+       self.song_4 = Song("Don't Fear the Repo", "Blue Oyster Cult")
 
     def test_room_has_name(self):
         self.assertEqual("Lizard Lounge", self.room_1.name)
@@ -30,3 +35,9 @@ class TestRoom(unittest.TestCase):
         self.room_1.check_out_guest(self.guest_1)
         self.assertEqual(2, len(self.room_1.guest_list))
         
+    def test_can_add_songs_to_room(self):
+        self.room_1.add_song_to_song_list(self.song_1)
+        self.room_1.add_song_to_song_list(self.song_2)
+        self.room_1.add_song_to_song_list(self.song_3)
+        self.room_1.add_song_to_song_list(self.song_4)
+        self.assertEqual(4, len(self.room_1.song_list))
